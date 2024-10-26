6969b06127e97a35d8075703f76da38384fc5fbe
SpaceScape <71204304+Space-Scape@users.noreply.github.com>
2024-10-26 00:04:58 -0500

    Update region_role_assign.py

--- region_role_assign.py
+++ region_role_assign.py
@@ -34,7 +34,7 @@ async def area_panel(ctx):
 
     # Upload the image and get the URL
     file = discord.File("https://cdn.discordapp.com/attachments/1273094409432469605/1299599252829503508/xejylXb.png?ex=671dc99f&is=671c781f&hm=ddb7ebcf4e96205dc6dca40b3a93f29411990fb26b7afb591c4cca2fbdce14cf&", filename="xejylXb.png")  # Use the path where your image is stored
-    embed.set_image(url="attachment://image.png")  # Reference the uploaded image as an attachment
+    embed.set_image(url="attachment://xejylXb.png")  # Reference the uploaded image as an attachment
 
 
     guild = ctx.guild
