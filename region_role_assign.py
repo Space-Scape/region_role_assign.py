9edc776fb7f9d8d38430afdd523dce58c10b77f5
SpaceScape <71204304+Space-Scape@users.noreply.github.com>
2024-10-26 00:04:36 -0500

    Update region_role_assign.py

--- region_role_assign.py
+++ region_role_assign.py
@@ -30,9 +30,12 @@ async def area_panel(ctx):
                     "\nDe-select to remove the area.",
         color=discord.Color.green()
     )
-    embed.set_image(url="https://imgur.com/a/J0KuMSy")
-    
     embed.set_footer(text="Select your areas")
+
+    # Upload the image and get the URL
+    file = discord.File("https://cdn.discordapp.com/attachments/1273094409432469605/1299599252829503508/xejylXb.png?ex=671dc99f&is=671c781f&hm=ddb7ebcf4e96205dc6dca40b3a93f29411990fb26b7afb591c4cca2fbdce14cf&", filename="xejylXb.png")  # Use the path where your image is stored
+    embed.set_image(url="attachment://image.png")  # Reference the uploaded image as an attachment
+
 
     guild = ctx.guild
     custom_emoji_desert = discord.utils.get(guild.emojis, name="desert")
